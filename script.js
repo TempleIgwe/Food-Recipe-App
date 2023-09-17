12994b9b94adb4afcd638749444f9a51b2c90ced
Temple Igwe <temple.igw@gmail.com>
2023-09-17 21:21:15 +0100

    Create Variables

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ const mealDetailsContent = document.querySelector(".meal-details-content");
 const recipeCloseBtn = document.getElementById("recipe-close-btn");
 
 searchBtn.addEventListener("click", getMealList);
+
+function getMealList() {
+  let searchInputTxt = document.getElementById("search-input").value.trim();
+  fetch("www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+}
