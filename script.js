3c3467d8313721b05b05cbd2bc31f62dd5551f16
Temple Igwe <temple.igw@gmail.com>
2023-09-18 12:03:36 +0100

    Add API

--- script.js
+++ script.js
@@ -4,12 +4,56 @@ const mealDetailsContent = document.querySelector(".meal-details-content");
 const recipeCloseBtn = document.getElementById("recipe-close-btn");
 
 searchBtn.addEventListener("click", getMealList);
+mealList.addEventListener("click", getMealRecipe);
 
 function getMealList() {
   let searchInputTxt = document.getElementById("search-input").value.trim();
-  fetch("www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast")
+  fetch(
+    `https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`
+  )
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
+      let html = "";
+      if (data.meals) {
+        data.meals.forEach((meal) => {
+          html += `
+            <div class="meal-item" data-id = "${meal.idMeal}">
+              <div class="meal-img">
+                <img
+                  src="${meal.strMealThumb}"
+                  alt="food"
+                />
+              </div>
+              <div class="meal-name">
+                <h3>${meal.strMeal}</h3>
+                <a href="#" class="recipe-btn">Get Recipe</a>
+              </div>
+            </div>
+        `;
+        });
+        mealList.classList.remove("notFound");
+      } else {
+        html = "Sorry, we didnt find any meal!";
+        mealList.classList.add("notFound");
+      }
+
+      mealList.innerHTML = html;
     });
+}
+
+function getMealRecipe(e) {
+  e.preventDefault();
+  if (e.target.classList.contains("recipe-btn")) {
+    let mealItem = e.target.parentElement.parentElement;
+    fetch(
+      `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`
+    )
+      .then((response) => response.json())
+      .then((data) => mealRecipeModal(data.meals));
+  }
+}
+
+function mealRecipeModal(meal) {
+  console.log(meal);
+  meal = meal[0];
 }
